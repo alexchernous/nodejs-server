c125b2c1076c82257a33e63a8cd5e89907e2f7d4
unknown <alex.a.chernous@gmail.com>
2020-11-05 23:04:06 -0500

    External fruit json database

--- server.js
+++ server.js
@@ -1,39 +1,52 @@
 'use strict';
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var cors = require('cors');
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const libPath = require('path');
+const fs = require('fs');
+const util = require('util');
 
-var corsOptions = {
+const app = express();
+const corsOptions = {
   origin: 'http://localhost:3000',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+};
+var _lastChangedFruitTimestamp = undefined;
+var _fruitData = undefined;
+var _fruitList = undefined;
 
 app.use(cors(corsOptions));
-app.use(express.static(__dirname + '/'));
+// app.use(express.static(__dirname + '/'));
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
   extended: true
 }));
 
-var fruits = {
-  apple: {
-    color: 'green'
-  },
-  orange: {
-    color: 'orange'
-  },
-  strawberry: {
-    color: 'red'
-  },
-  blueberry: {
-    color: 'blue'
+app.use(async (req, res, next) => {
+  const fruitsBaseFilePath = libPath.join(__dirname, 'src/database/fruits.json');
+
+  try {
+    const fsStatAsync = util.promisify(fs.stat);
+    const fsReadFileAsync = util.promisify(fs.readFile);
+
+    const fruitFileStat = await fsStatAsync(fruitsBaseFilePath);
+
+    if (!_lastChangedFruitTimestamp || (_lastChangedFruitTimestamp !== fruitFileStat.ctimeMs)) {
+      _lastChangedFruitTimestamp = fruitFileStat.ctimeMs;
+      const fruitFileContent = await fsReadFileAsync(fruitsBaseFilePath, 'utf-8');
+
+      _fruitData = JSON.parse(fruitFileContent);
+    }
+  } catch (error) {
+    console.log(`Can't grab fruit database: ${error}`);
   }
-};
 
-var fruitList = Object.keys(fruits);
+  _fruitList = Object.keys(_fruitData);
+  next();
+});
 
+// Routes
 app.get('/', function (req, res) {
   res.json({
     'Fruit service status': 'RUNNING'
@@ -41,19 +54,29 @@ app.get('/', function (req, res) {
 });
 
 app.get('/fruit/fruitList', function (req, res) {
-  res.send(JSON.stringify(fruitList));
+  res.send(JSON.stringify(_fruitList));
 });
 
 app.get('/fruit/:id', function (req, res) {
-  res.send(JSON.stringify(fruits[req.params.id]));
+  res.send(JSON.stringify(_fruitData[req.params.id]));
 });
 
 app.post('/fruit', function (req, res) {
-  fruits[req.body.name] = req.body.content;
-  res.send(JSON.stringify(fruits));
+  let data = {};
+  try {
+    _fruitData = {..._fruitData, ...req.body};
+
+    const fsWriteFileAsync = util.promisify(fs.writeFile);
+    fsWriteFileAsync(libPath.join(__dirname, 'src/database/fruits.json'), JSON.stringify(_fruitData));
+
+    res.status(200).send({ content: [JSON.stringify(_fruitData), req.body] });
+  } catch (error) {
+    console.log(`Can't write to fruit database: ${error}`);
+    res.sendStatus(500);
+  }
 });
 
-// start server
+// Start server
 if (!module.parent) {
   const port = process.env.PORT || 3001;
 
